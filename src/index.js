33e56be8fc86c8f0be12b28cb034e1c9bc317ae2
Mark Quinn <mark@playcircl.com>
2024-03-07 17:38:07 +0000

    sending email template

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
+require('dotenv').config({ path: '../.env' });
+
+const nodemailer = require('nodemailer');
+const hbs = require('nodemailer-express-handlebars');
 let Parser = require('rss-parser');
 let parser = new Parser();
 const puppeteer = require('puppeteer');
 let OpenAI = require('openai');
-const config = require('../config');
 
 const openai = new OpenAI({
-	apiKey: config.chatGPTApiKey,
+	apiKey: process.env.CHAT_GPT_API_KEY,
 });
 
 async function run() {
@@ -14,13 +17,13 @@ async function run() {
 			name: 'BBC Football',
 			url: 'http://newsrss.bbc.co.uk/rss/sportonline_uk_edition/football/rss.xml',
 			type: 'BBC',
-			articleCount: 1,
+			articleCount: 5,
 		},
 		{
 			name: 'BBC News UK',
 			url: 'http://newsrss.bbc.co.uk/rss/newsonline_uk_edition/front_page/rss.xml',
 			type: 'BBC',
-			articleCount: 1,
+			articleCount: 5,
 		},
 		{
 			name: 'Sky Sports News',
@@ -31,41 +34,48 @@ async function run() {
 	];
 
 	const browser = await puppeteer.launch();
-	const page = await browser.newPage();
+
+	const promises = [];
 
 	for (const feed of feeds) {
 		switch (feed.type) {
 			case 'BBC':
-				await parseBBCFeed(feed, page);
+				promises.push(parseBBCFeed(feed, browser));
 				break;
 			default:
 				console.error(`No parser for ${feed.type}`);
 		}
 	}
 
+	const results = await Promise.all(promises);
+
+	await sendEmail(results);
+
 	await browser.close();
 }
 
-async function parseBBCFeed(feed, page) {
-	console.log(feed.name, '\n\n');
+async function parseBBCFeed(feed, browser) {
 	let parsedFeed = await parser.parseURL(feed.url);
 
-	for (const item of parsedFeed.items.slice(0, feed.articleCount)) {
-		console.log(item.link);
-		console.log(item.title);
+	const promises = [];
 
+	for (const item of parsedFeed.items.slice(0, feed.articleCount)) {
+		const page = await browser.newPage();
 		await page.goto(item.link, { waitUntil: 'networkidle2' });
 
 		const article = await page.evaluate(() => {
 			return [...document.querySelectorAll('article')][0].textContent;
 		});
 
-		await summarise(article);
+		promises.push(summarise(article, item));
 	}
+
+	const results = await Promise.all(promises);
+
+	return { feedName: feed.name, articles: results };
 }
 
-async function summarise(content) {
-	console.log('Summarising...');
+async function summarise(content, item) {
 	const completion = await openai.chat.completions.create({
 		messages: [
 			{ role: 'system', content: 'You are a helpful assistant.' },
@@ -74,7 +84,42 @@ async function summarise(content) {
 		model: 'gpt-3.5-turbo',
 	});
 
-	console.log(completion.choices[0].message.content, '\n\n\n\n');
+	return {
+		link: item.link,
+		title: item.title,
+		content: completion.choices[0].message.content,
+	};
+}
+
+async function sendEmail(results) {
+	const transporter = nodemailer.createTransport({
+		service: 'gmail',
+		auth: {
+			user: process.env.GMAIL_EMAIL,
+			pass: process.env.GMAIL_PASSWORD,
+		},
+	});
+
+	const hbsOptions = {
+		viewEngine: {
+			defaultLayout: false,
+		},
+		viewPath: 'templates',
+	};
+
+	transporter.use('compile', hbs(hbsOptions));
+
+	const mailOptions = {
+		from: process.env.GMAIL_EMAIL,
+		to: process.env.GMAIL_EMAIL,
+		subject: 'News Blast',
+		template: 'news_blast',
+		context: { blocks: results },
+	};
+
+	await transporter.sendMail(mailOptions);
+
+	console.log('Email Sent!');
 }
 
 run();
